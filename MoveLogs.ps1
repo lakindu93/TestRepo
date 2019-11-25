9f291d78229240ed7d700a8d7aa3bdc26f03428a
Lakindu <lakindu93@gmail.com>
2019-11-25 22:09:27 +0530

    Add my project

---
+++ MoveLogs.ps1
@@ -0,0 +1,16 @@
+﻿$server="DC01"
+
+$source="\\$server\C$\Windows\System32\winevt\Logs\Archive-Security*.evtx"
+
+$date = Get-Date
+
+$yr_mo = $date.Year.ToString()+"_"+$date.Month.ToString()
+
+
+if(Test-Path $source){
+    Write-Host Logs are Ready
+}
+else{
+    Write-Host No Logs
+}
+
